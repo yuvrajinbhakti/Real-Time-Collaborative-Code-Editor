09ae27c2e203eea2ca2a6f1ba752434ab8153751
yuvrajinbhakti <yuvrajsinghnain03@gmail.com>
2024-04-27 17:23:26 +0530

    added validation and also added functionality to join room by pressing enter key after entering room id and username

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
 import React,{useState} from 'react'
 import {v4 as uuidv4} from 'uuid';
 import toast from 'react-hot-toast';
+import {useNavigate} from 'react-router-dom';
 
 const Home = () => {
 
+  const navigate=useNavigate();
+
   const [roomId,setRoomId]=useState('');
-  const [Username,setUsername]=useState('');
+  const [username,setUsername]=useState('');
 
     const createNewRoom=(e)=>{
   e.preventDefault();
@@ -13,6 +16,26 @@ const Home = () => {
   setRoomId(id);
   toast.success('Created a new room.')
   };
+
+  const joinRoom=()=>{
+if(!roomId || !username){
+  toast.error('ROOM ID & Username is required');
+  return;
+}
+
+//redirecting if both username and room id is given
+navigate(`/editor/${roomId}`,{
+state:{
+  username,
+},
+});
+  };
+
+  const handleInputEnter=(e)=>{
+if(e.code === 'Enter'){
+joinRoom();
+}
+  }
 
   return (
     <div className="homePageWrapper">
@@ -22,17 +45,20 @@ const Home = () => {
  alt="code-sync-logo"></img>
  <h4 className='mainLabel'>Paste invitation ROOM ID</h4>
 <div className='inputGroup'>
+
 <input type='text' className='inputBox' placeholder='ROOM ID' 
 onChange={(e)=>setRoomId(e.target.value)}
 value={roomId}
+onKeyUp={handleInputEnter}
 />
 
 <input type='text' className='inputBox' placeholder='USERNAME' 
 onChange={(e)=>setUsername(e.target.value)}
-value={Username}
+value={username}
+onKeyUp={handleInputEnter}
 />
 
-<button className='btn joinBtn'>JOIN</button>
+<button className='btn joinBtn' onClick={joinRoom}>JOIN</button>
 
 <span className='createInfo'>
 If you don't have an invite then create &nbsp;
