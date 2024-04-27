00a399b16dd2f8fa872f5d8074339f91db6f4f75
yuvrajinbhakti <yuvrajsinghnain03@gmail.com>
2024-04-27 16:49:45 +0530

    done till generating unique room id

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {v4 as uuidv4} from 'uuid';
 
 const Home = () => {
+
+  const [roomId,setRoomId]=useState('');
+
+    const createNewRoom=(e)=>{
+  e.preventDefault();
+  const id=uuidv4();
+  setRoomId(id);
+  };
+
   return (
     <div className="homePageWrapper">
       <div className="formWrapper">
@@ -9,15 +19,17 @@ const Home = () => {
  alt="code-sync-logo"></img>
  <h4 className='mainLabel'>Paste invitation ROOM ID</h4>
 <div className='inputGroup'>
-<input type='text' className='inputBox' placeholder='ROOM ID' />
+<input type='text' className='inputBox' placeholder='ROOM ID' 
+value={roomId}
+/>
 
 <input type='text' className='inputBox' placeholder='USERNAME' />
 
-<button className='btn joinbtn'>JOIN</button>
+<button className='btn joinBtn'>JOIN</button>
 
 <span className='createInfo'>
 If you don't have an invite then create &nbsp;
-<a href="" className='createNewBtn'>New Room</a>
+<a onClick={createNewRoom} href="" className='createNewBtn'>New Room</a>
 </span>
 
 </div>
